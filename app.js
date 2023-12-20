0effbda6170814f69d9bc9b3ca4eced4ef2aefd8
Vidya M Gowda <116086874+vidyashri123@users.noreply.github.com>
2023-12-20 22:46:31 +0530

    Update script.js

--- app.js
+++ app.js
@@ -1,110 +1,191 @@
-const questions = [
+const quizData = [
     {
-        question: "What is the capital of France?",
-        answers: [
-            { text: "Berlin", correct: false },
-            { text: "Madrid", correct: false },
-            { text: "Paris", correct: true },
-            { text: "Rome", correct: false }
-        ]
+      question: 'What is the capital of France?',
+      options: ['Paris', 'London', 'Berlin', 'Madrid'],
+      answer: 'Paris',
     },
     {
-        question: "Which planet is known as the Red Planet?",
-        answers: [
-            { text: "Earth", correct: false },
-            { text: "Mars", correct: true },
-            { text: "Venus", correct: false },
-            { text: "Jupiter", correct: false }
-        ]
+      question: 'What is the largest planet in our solar system?',
+      options: ['Mars', 'Saturn', 'Jupiter', 'Neptune'],
+      answer: 'Jupiter',
     },
     {
-        question: "What is the largest mammal?",
-        answers: [
-            { text: "Elephant", correct: false },
-            { text: "Whale Shark", correct: false },
-            { text: "Blue Whale", correct: true },
-            { text: "Giraffe", correct: false }
-        ]
+      question: 'Which country won the FIFA World Cup in 2018?',
+      options: ['Brazil', 'Germany', 'France', 'Argentina'],
+      answer: 'France',
     },
     {
-        question: "Which programming language is known for building dynamic web pages?",
-        answers: [
-            { text: "Java", correct: false },
-            { text: "Python", correct: false },
-            { text: "JavaScript", correct: true },
-            { text: "C++", correct: false }
-        ]
+      question: 'What is the tallest mountain in the world?',
+      options: ['Mount Everest', 'K2', 'Kangchenjunga', 'Makalu'],
+      answer: 'Mount Everest',
     },
     {
-        question: "Who developed the theory of general relativity?",
-        answers: [
-            { text: "Isaac Newton", correct: false },
-            { text: "Galileo Galilei", correct: false },
-            { text: "Albert Einstein", correct: true },
-            { text: "Stephen Hawking", correct: false }
-        ]
+      question: 'Which is the largest ocean on Earth?',
+      options: [
+        'Pacific Ocean',
+        'Indian Ocean',
+        'Atlantic Ocean',
+        'Arctic Ocean',
+      ],
+      answer: 'Pacific Ocean',
+    },
+    {
+      question: 'What is the chemical symbol for gold?',
+      options: ['Au', 'Ag', 'Cu', 'Fe'],
+      answer: 'Au',
+    },
+    {
+      question: 'Who painted the Mona Lisa?',
+      options: [
+        'Pablo Picasso',
+        'Vincent van Gogh',
+        'Leonardo da Vinci',
+        'Michelangelo',
+      ],
+      answer: 'Leonardo da Vinci',
+    },
+    {
+      question: 'Which planet is known as the Red Planet?',
+      options: ['Mars', 'Venus', 'Mercury', 'Uranus'],
+      answer: 'Mars',
+    },
+    {
+      question: 'What is the largest species of shark?',
+      options: [
+        'Great White Shark',
+        'Whale Shark',
+        'Tiger Shark',
+        'Hammerhead Shark',
+      ],
+      answer: 'Whale Shark',
+    },
+    {
+      question: 'Which animal is known as the King of the Jungle?',
+      options: ['Lion', 'Tiger', 'Elephant', 'Giraffe'],
+      answer: 'Lion',
+    },
+  ];
+  
+  const quizContainer = document.getElementById('quiz');
+  const resultContainer = document.getElementById('result');
+  const submitButton = document.getElementById('submit');
+  const retryButton = document.getElementById('retry');
+  const showAnswerButton = document.getElementById('showAnswer');
+  
+  let currentQuestion = 0;
+  let score = 0;
+  let incorrectAnswers = [];
+  
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
-    // Add more questions as needed
-];
-
-const questionsLimit = 5; // Set the number of questions to display
-
-let currentQuestionIndex = 0;
-let score = 0;
-
-const questionElement = document.getElementById("question");
-const answerButtonsElement = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-button");
-
-function startQuiz() {
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.classList.add("hide");
-    showQuestion(questions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question;
-    resetAnswerButtons();
-    question.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerText = answer.text;
-        button.classList.add("btn");
-        button.addEventListener("click", () => selectAnswer(answer));
-        answerButtonsElement.appendChild(button);
-    });
-}
-
-function resetAnswerButtons() {
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+  
+  function displayQuestion() {
+    const questionData = quizData[currentQuestion];
+  
+    const questionElement = document.createElement('div');
+    questionElement.className = 'question';
+    questionElement.innerHTML = questionData.question;
+  
+    const optionsElement = document.createElement('div');
+    optionsElement.className = 'options';
+  
+    const shuffledOptions = [...questionData.options];
+    shuffleArray(shuffledOptions);
+  
+    for (let i = 0; i < shuffledOptions.length; i++) {
+      const option = document.createElement('label');
+      option.className = 'option';
+  
+      const radio = document.createElement('input');
+      radio.type = 'radio';
+      radio.name = 'quiz';
+      radio.value = shuffledOptions[i];
+  
+      const optionText = document.createTextNode(shuffledOptions[i]);
+  
+      option.appendChild(radio);
+      option.appendChild(optionText);
+      optionsElement.appendChild(option);
     }
-}
-
-function selectAnswer(answer) {
-    if (answer.correct) {
+  
+    quizContainer.innerHTML = '';
+    quizContainer.appendChild(questionElement);
+    quizContainer.appendChild(optionsElement);
+  }
+  
+  function checkAnswer() {
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    if (selectedOption) {
+      const answer = selectedOption.value;
+      if (answer === quizData[currentQuestion].answer) {
         score++;
+      } else {
+        incorrectAnswers.push({
+          question: quizData[currentQuestion].question,
+          incorrectAnswer: answer,
+          correctAnswer: quizData[currentQuestion].answer,
+        });
+      }
+      currentQuestion++;
+      selectedOption.checked = false;
+      if (currentQuestion < quizData.length) {
+        displayQuestion();
+      } else {
+        displayResult();
+      }
     }
-    showNextButton();
-}
-
-function showNextButton() {
-    nextButton.classList.remove("hide");
-}
-
-function nextQuestion() {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questionsLimit) {
-        showQuestion(questions[currentQuestionIndex]);
-        nextButton.classList.add("hide");
-    } else {
-        endQuiz();
+  }
+  
+  function displayResult() {
+    quizContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    retryButton.style.display = 'inline-block';
+    showAnswerButton.style.display = 'inline-block';
+    resultContainer.innerHTML = `You scored ${score} out of ${quizData.length}!`;
+  }
+  
+  function retryQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    incorrectAnswers = [];
+    quizContainer.style.display = 'block';
+    submitButton.style.display = 'inline-block';
+    retryButton.style.display = 'none';
+    showAnswerButton.style.display = 'none';
+    resultContainer.innerHTML = '';
+    displayQuestion();
+  }
+  
+  function showAnswer() {
+    quizContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    retryButton.style.display = 'inline-block';
+    showAnswerButton.style.display = 'none';
+  
+    let incorrectAnswersHtml = '';
+    for (let i = 0; i < incorrectAnswers.length; i++) {
+      incorrectAnswersHtml += `
+        <p>
+          <strong>Question:</strong> ${incorrectAnswers[i].question}<br>
+          <strong>Your Answer:</strong> ${incorrectAnswers[i].incorrectAnswer}<br>
+          <strong>Correct Answer:</strong> ${incorrectAnswers[i].correctAnswer}
+        </p>
+      `;
     }
-}
-
-function endQuiz() {
-    questionElement.innerText = `You completed the quiz! Your score is ${score} out of ${questionsLimit}.`;
-    resetAnswerButtons();
-}
-
-startQuiz();
+  
+    resultContainer.innerHTML = `
+      <p>You scored ${score} out of ${quizData.length}!</p>
+      <p>Incorrect Answers:</p>
+      ${incorrectAnswersHtml}
+    `;
+  }
+  
+  submitButton.addEventListener('click', checkAnswer);
+  retryButton.addEventListener('click', retryQuiz);
+  showAnswerButton.addEventListener('click', showAnswer);
+  
+  displayQuestion();
